e14a934245f5c6c1c405ca384cc1bf33e7830a0b
rebehk <rebehk.jordao@gmail.com>
2022-08-29 03:46:01 -0300

    adicioando metodos de busca e like

--- controller/neo4jdbEntity.js
+++ controller/neo4jdbEntity.js
@@ -2,17 +2,17 @@ const neo4j = require("../database/neodb");
 const { request, response } = require("express");
 
 const addPessoa = async (request, response) => {
-    try {
-        const session = neo4j.gdbdriver.session();
+  try {
+    const session = neo4j.gdbdriver.session();
 
-        await session.run(`CREATE (:Pessoa{email:"${request.body.email}", 
+    await session.run(`CREATE (:Pessoa{email:"${request.body.email}", 
                 nome:"${request.body.nome}" })`);
-        await session.close();
-
-      response.status(200).send("Salvo com sucesso");
-    } catch (err) {
-      response.status(500).send({ error: err.message });
-    }
+    await session.close();
+    console.log(request.body);
+    response.status(200).send("Salvo com sucesso");
+  } catch (err) {
+    response.status(500).send({ error: err.message });
+  }
 }
 
 const favoritarDispositivo = async (request, response) => {
@@ -23,6 +23,7 @@ const favoritarDispositivo = async (request, response) => {
       create (p1)-[:LIKE]->(d1)`);
   if (result.summary.counters._stats.relationshipsCreated > 0) {
     response.status(200).send("Favorito adicionado.");
+    console.log(request.body);
   } else {
     response.status(400).send("Falha ao favoritar.");
   }
@@ -40,27 +41,29 @@ function formatResponse(resultObj) {
 }
 
 //PARA SABER SE UM USUARIO ESTA NO NEO4J
-const getUsuario =  async function(request, response) {
-    const session = neo4j.gdbdriver.session();
-    const query = `match(p1:Pessoa{email:"${request.params.nome}"}) return p1`
-    const resultObj = await session.run(query);
-    const result = formatResponse(resultObj);
-    await session.close();
-    response.status(200).send(result);
+const getUsuario = async function (request, response) {
+  const session = neo4j.gdbdriver.session();
+  const query = `match(p1:Pessoa{nome:"${request.params.nome}"}) return p1`
+  const resultObj = await session.run(query);
+  const result = formatResponse(resultObj);
+  console.log(result)
+  await session.close();
+  response.status(200).send(result);
 };
 
-const buscarFavoritadosPorUmUsuario =  async (request, response) => {
-    const session = neo4j.gdbdriver.session();
-    const query = `MATCH (n:Pessoa {email: "${request.params.email}"})-[:LIKE]-(d:Dispositivo) RETURN d`;
-    const resultObj = await session.run(query);
-    const result = formatResponse(resultObj);
-    await session.close();
-    response.status(200).send(result);
+const buscarFavoritadosPorUmUsuario = async (request, response) => {
+  const session = neo4j.gdbdriver.session();
+  const query = `MATCH (n:Pessoa {email: "${request.params.email}"})-[:LIKE]-(d:Dispositivo) RETURN d`;
+  const resultObj = await session.run(query);
+  const result = formatResponse(resultObj);
+  console.log(result)
+  await session.close();
+  response.status(200).send(result);
 };
 
-module.exports = { 
-    addPessoa,
-    getUsuario,
-    favoritarDispositivo,
-    buscarFavoritadosPorUmUsuario
+module.exports = {
+  addPessoa,
+  getUsuario,
+  favoritarDispositivo,
+  buscarFavoritadosPorUmUsuario
 }
--- pages/front.js
+++ pages/front.js
@@ -1,12 +1,14 @@
 const baseURL = 'http://localhost:3000'
 let dispositivos = []
 
+
 //fetch methods
 function carregaDispositivos() {
     fetch(`${baseURL}/inicio`)
         .then(response => response.json())
         .then(json => {
             dispositivos = json
+            console.log(dispositivos)
             renderizaNaTela()
         })
         .catch(err => false)
@@ -239,3 +241,190 @@ function handleSetModal(modelo, idElem) {
         limparModalForm()
     }
 }
+
+// neo4j 
+function renderizaNaTelaFavorito(fav) {
+    const div = document.querySelector(".container-pg2")
+    for (let tam = 0; tam < array.length; tam++) {
+        const { modelof } = fav
+        console("passou pelo for ")
+
+        div.innerHTML = ''
+        if (dispositivos.length) {
+            dispositivos.forEach(dispositivo => {
+                console.log('forEach')
+                const {
+                    fotoLink,
+                    modelo,
+                    fabricante,
+                    preco,
+                    processador,
+                    memoriaInterna,
+                    memoriaRam
+                } = dispositivo
+                if (modelof == modelo) {
+                    console(modelo, "passando pelo If")
+                    const html = `
+                <div class="card cards" style="width: 18rem;">
+                <div class="img-div">
+                <img src=${fotoLink} class=" img-tam" alt="...">
+                </div>
+                <div class="card-body">
+                <h5 class="card-title txt-cent">${modelo}</h5>
+                <p class="card-text">fabricante: ${fabricante}</p>
+                <p class="card-text">preço: ${preco}</p>
+                <p class="card-text">processador: ${processador}</p>
+                <p class="card-text">memoria interna: ${memoriaInterna}</p>
+                <p class="card-text">memoria ram: ${memoriaRam}</p>
+                <div class="btn-container">
+                <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Opções
+                        </button>
+                        <ul class="dropdown-menu">
+                        </button></li>
+                        <li><button class="btn-edit" onclick="handleSetModal('${modelo}', 'modalForm')" data-bs-toggle="modal" data-bs-target="#modalForm">
+                        Editar
+                        </button></li>
+                        <li><button onclick="handleSetModal('${modelo}', 'delete')" class="btn-del" data-bs-toggle="modal" data-bs-target="#delete">
+                        Excluir
+                        </button></li>
+                        </ul>
+                        </div>
+                        <button onclick="handleSetModal('${modelo}', 'user')" class="btn-like" data-bs-toggle="modal" data-bs-target="#user-like">
+                        <img src="./assets/estrela.png" height ="35" width="40"/>
+                        </button> 
+                    </div>
+                    </div>
+                    </div>
+                    `
+                    div.innerHTML += html
+                }
+            })
+
+        }
+        // else {
+        //     div.innerHTML = `
+        //     <div class="card cards" style="width: 18rem;">
+        //             <div class="card-body">
+        //             <h5 class="card-title">Nenhum dispositivo favoritado</h5>
+        //         </div>
+        //     </div>
+        // `
+        // }
+
+    }
+}
+
+function handleAddUser(pessoa) {
+    fetch(`${baseURL}/adicionar_pessoa`, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(pessoa)
+    }).then(response => {
+        console.log()
+    })
+        .catch(err => false)
+}
+
+function listarDispositivosFavoritos() {
+    let email = document.querySelector('#name').value
+    let favoritado = []
+    fetch(`${baseURL}/listar_dispositivos_favoritados_por/${email}`)
+        .then(response => response.json())
+        .then(fav => {
+            favoritado = fav
+            renderizaNaTelaFavorito()
+            console.log(favoritado)
+
+        })
+        .catch(err => false)
+}
+
+// function buscarFavorito() {
+//     let email = document.querySelector('#name').value
+//     listarDispositivosFavoritos(email)
+//     limparModalAddPessoa()
+// }
+
+
+// try {
+//     const response = await fetch(`${baseURL}/busca/${modelo}`)
+//     const dispositivo = await response.json()
+
+//     return dispositivo ? dispositivo : false
+// } catch (err) {
+//     return false
+// }
+
+
+
+function favoritarDispositivo(like) {
+    fetch(`${baseURL}/favoritar_dispositivo`, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(like)
+    }).then(response => {
+        console.log()
+    })
+        .catch(err => false)
+}
+
+
+function buscaPessoaModalForm() {
+    return {
+        nome: document.querySelector('#nome').value,
+        email: document.querySelector('#email').value,
+
+    }
+}
+function limparModalAddPessoa() {
+    document.querySelector('#nome').value = ''
+    document.querySelector('#name').value = ''
+    document.querySelector('#email').value = ''
+    document.querySelector('#email-exist').value = ''
+    document.querySelector('#modelo-like1').value = ''
+
+
+}
+
+
+
+function handleModalAddUser() {
+    let pessoa = buscaPessoaModalForm()
+    handleAddUser(pessoa)
+    limparModalAddPessoa()
+}
+
+// function buscarUsuario(usuario) {
+//     fetch(`${baseURL}/busca/${usuario}`, {
+//         method: 'POST',
+//         headers: {
+//             'accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(like)
+//     }).then(response => {
+//         console.log()
+//     })
+//         .catch(err => false)
+
+// }
+
+
+
+function handleModalUser() {
+    let like = {}
+    like.email = document.querySelector('#email-exist').value
+    like.modelo = document.querySelector('#modelo-like1').value
+    console.log(like)
+    favoritarDispositivo(like)
+    limparModalAddPessoa()
+
+} 
